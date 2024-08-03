a8d160b32bd924e9cd672bcdca2c642898b91072
Savita <96195601+savigurjar@users.noreply.github.com>
2024-08-03 23:35:51 +0530

    Add files via upload

---
+++ sorting/selectionsort/selesortdecrease.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int selectionSort(int n, vector<int>& arr)
+{
+	for(int i=0;i<n-1;i++)
+	{
+		int index = i;
+		for(int j=i+1;j<n;j++)
+		{
+			if(arr[j]>arr[index])
+			{
+				index = j;
+			}
+		}
+		swap(arr[index],arr[i]);
+	}
+}
+int main()
+{
+	int n;
+	cout<<"enter size of array ";
+	cin>>n;
+	
+	vector<int>arr(1000);
+	cout<<"enter element in array ";
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	
+	selectionSort(n,arr);
+	cout<<"sorted array by using selection sort decreasing order ";
+	for(int i=0;i<n;i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+	
+	return 0;
+}
